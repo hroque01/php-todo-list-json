bd946c396a85c24a192e57c1d86fd76866217680
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-01-13 20:23:26 +0100

    Create api_newtask.php

---
+++ backend/api_create_new_task.php
@@ -0,0 +1,25 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+header('Content-Type: application/json');
+
+//prendiamo il 'text' dell'array di toDo.php
+$text = $_GET['text'];
+
+//prendi i contenuti di todo.json
+$jsonTodoList = file_get_contents("todo.json");
+
+//prendo l'array dentro sopra e ci metto la variabile appena creata
+$todoList = json_decode($jsonTodoList);
+
+//inserisco nell'array
+$todoList[] = [
+    "text" => $text,
+    "complleted" => false
+];
+
+// implimentiamo il nuovo array nel todolist di todo.json
+$jsonTodoList = json_encode($todoList);
+file_put_contents("todo.json", $jsonTodoList);
